3f754a866c409e39c4872f9e8fa2220a00871b28
Lavakumarg <lavakum68@gmail.com>
2026-03-04 11:54:40 +0530

    Update babel.js
    bable update

--- balel/babel.js
+++ balel/babel.js
@@ -2,17 +2,17 @@ let unme = "lava"
 let greet = <h1>good morning {unme} </h1>
 let hobbies =["travelling","dancing","swimming","talking"]
 
-// let hob = <ul>
-//     <li>travelling</li>
-//     <li>dancing</li>
-//     <li>swimming</li>
-//     <li>talking</li>
-// </ul>
+let hob = <ul>
+    <li>travelling</li>
+    <li>dancing</li>
+    <li>swimming</li>
+    <li>talking</li>
+</ul>
 
-let hobb = hobbies.map((hobb, ind) =>{
-    return <li>{hobb}</li>
-});
+// let hobb = hobbies.map((hobb, ind) =>{
+//     return <li>{hobb}</li>
+// });
 
 
 // let mainele = React.createElement("div", null,greet, hob);
-ReactDOM.render(<div>{greet} {hobb}</div>, document.getElementById("root"));
+ReactDOM.render(<div>{greet} {hob}</div>, document.getElementById("root"));
