1ea06e7cea0955bd45cb3decd848635a9866addb
Lavakumar86 <lavakum68@gmail.com>
2026-03-05 00:07:12 +0530

    addition of logical folder and files

---
+++ logicsTask/tooggleSwitch.js
@@ -0,0 +1,39 @@
+function AdmininUserView(){
+    const[adminuser, setAdminUser] = React.useState({
+        adminName: "jhon",
+        userName: "David",
+        isAdmin : true   //setting up the flag
+    });
+
+    let renderData;
+    const {adminName, userName, isAdmin}= adminuser; //destructuring
+    if(adminuser.isAdmin){
+        renderData =(
+            <div>
+                <h1>hello admin{adminName}</h1>
+                <h1>home page</h1>
+                <h1>product page</h1>
+                <h1>product details</h1>
+            </div>
+        );
+    }
+    else{
+        renderData =(
+            <div>
+                <h1>hello admin{Useradmin}</h1>
+                <h1>home page</h1>
+                <h1>product page</h1>
+                <h1>product details</h1>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            {renderData}
+            <button> onClick = {() => setAdminUser({ ...adminuser, isAdmin : !isAdmin})}Switch to{isAdmin ? `user` : `admin`}view</button>
+        </div>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<AdmininUserView />)
